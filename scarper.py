b178e10c28d51c53f0f07fdd29da3506e3b58c6c
yash-dani <daniyash19@gmail.com>
2020-07-08 22:16:11 -0400

    added more filters to scraper, created test output

--- scarper.py
+++ scarper.py
@@ -8,12 +8,31 @@ def test():
 	response = requests.get("https://www.arxiv-vanity.com/papers/1603.09382/")
 	soup = BeautifulSoup(response.content, 'html.parser')
 
-	# remove script, style, and head tags
-	[x.extract() for x in soup.findAll('script')]
-	[x.extract() for x in soup.findAll('style')]
-	[x.extract() for x in soup.findAll('head')]
+	# remove unneeded tags
+	[elem.extract() for elem in soup.findAll('script')]
+	[elem.extract() for elem in soup.findAll('style')]
+	[elem.extract() for elem in soup.findAll('head')]
+	[elem.extract() for elem in soup.findAll('footer')]
+	[elem.extract() for elem in soup.findAll('form')]
+	[elem.extract() for elem in soup.findAll('div', {'class':'arxiv-vanity-wrapper'})]
+	# remove inline styles, classes, ids
+	for tag in soup.find_all():
+		if 'style' in tag.attrs: 
+			del tag.attrs['style']
+		if 'class' in tag.attrs:
+			del tag.attrs['class']
+		if 'id' in tag.attrs:
+			del tag.attrs['id']
 
-
-	print(type(soup.prettify()))
+	print(soup)
+	htmlFile = open('test.html', 'w')
+	a = htmlFile.write(soup.prettify())
+	htmlFile.close()
+	
+	'''
+	to remove:
+		<div class="arxiv-vanity-wrapper">
+		<div class="arxiv-vanity-wrapper">
+	'''
 
 test()
